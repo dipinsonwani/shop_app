7cc92324d670372cc36db2a59b05884d29acd6de
dipinsonwani <55944433+dipinsonwani@users.noreply.github.com>
2020-12-21 16:56:49 +0530

    Add Product model and ProductOverviewScreen

---
+++ lib/models/product.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavourite;
+
+  Product({
+    @required this.description,
+    @required this.id,
+    @required this.imageUrl,
+    @required this.price,
+    @required this.title,
+    this.isFavourite,
+  });
+}
