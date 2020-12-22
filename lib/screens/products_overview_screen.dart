c82ff49f8467d9a4856a9d78700e088fbd0cbb05
dipinsonwani <dipinsonwani@gmail.com>
2020-12-22 13:40:35 +0530

    Add GridView in ProductOverviewScreen

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -40,6 +40,18 @@ class ProductsOverviewScreen extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('Shop App')),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, i) => Container(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
+      ),
+    );
   }
 }
