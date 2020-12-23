181c43f6934045627e403f873a5c284f15f59f58
dipinsonwani <dipinsonwani@gmail.com>
2020-12-23 16:59:44 +0530

    Add listener to the ProductItem

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/providers/product.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
-
-  ProductItem(this.id, this.title, this.imageUrl);
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GestureDetector(
         onTap: () {
-          Navigator.of(context).pushNamed(ProductDetailScreen.routeName);
+          Navigator.of(context)
+              .pushNamed(ProductDetailScreen.routeName, arguments: product.id);
         },
         child: GridTile(
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
           footer: GridTileBar(
@@ -27,7 +26,7 @@ class ProductItem extends StatelessWidget {
               color: Theme.of(context).accentColor,
             ),
             title: Text(
-              title,
+              product.title,
               textAlign: TextAlign.center,
             ),
             backgroundColor: Colors.black54,
