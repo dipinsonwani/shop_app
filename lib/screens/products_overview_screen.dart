2bb901463501e353730eb819270f85eb8d27ff72
dipinsonwani <dipinsonwani@gmail.com>
2020-12-23 19:40:28 +0530

    Add ShowFavourites Popup menu in App bar

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
 import '../widgets/products_grid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FilterOptions { Favourites, All }
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavourites = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Shop App')),
+      appBar: AppBar(
+        title: Text('Shop App'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+            },
+            itemBuilder: (ctx) => [
+              PopupMenuItem(
+                child: Text("Only Favourites"),
+                value: FilterOptions.Favourites,
+              ),
+              PopupMenuItem(
+                child: Text("Show all"),
+                value: FilterOptions.All,
+              ),
+            ],
+          )
+        ],
+      ),
       body: ProductsGrid(),
     );
   }
 }
-
-
