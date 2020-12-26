2d483d2a57336ff2ed99bdd59a290151da5d4689
dipinsonwani <dipinsonwani@gmail.com>
2020-12-26 15:56:55 +0530

    Add CartScreen

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,37 @@
+import 'dart:html';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Cart'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'Total',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Chip(label: Text("totalAmount"),)
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
