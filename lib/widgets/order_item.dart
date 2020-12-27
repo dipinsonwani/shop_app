01a6b066f459c155c97e9a31c9b6b3e127a7a886
dipinsonwani <dipinsonwani@gmail.com>
2020-12-27 17:27:09 +0530

    Add Widget OrderItem

---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,31 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import '../providers/orders.dart' as ord;
+import 'package:intl/intl.dart';
+
+class OrderItem extends StatelessWidget {
+  final ord.OrderItem orders;
+
+  OrderItem(this.orders);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text('${orders.amount}'),
+            subtitle: Text(
+              DateFormat('dd MM yyyy hh:mm').format(orders.dateTime),
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.expand_more),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
