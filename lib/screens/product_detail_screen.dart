8f9756cdb00c4efaaffbe981ce6a37392532252b
dipinsonwani <dipinsonwani@gmail.com>
2020-12-27 14:54:40 +0530

    add UI in ProductDetailScreen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/products.dart';
@@ -10,10 +12,43 @@ class ProductDetailScreen extends StatelessWidget {
     //listening through provider
     final loadedProduct =
         Provider.of<Products>(context, listen: false).findById(productId);
-        //listen=false because there's no need to rebuild
+    //listen=false because there's no need to rebuild
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+              height: 400,
+              width: double.infinity,
+            ),
+            SizedBox(height: 10),
+            Text(
+              '₹${loadedProduct.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                loadedProduct.description,
+                style: TextStyle(),
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
