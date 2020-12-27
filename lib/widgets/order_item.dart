4d5441db6fff188321f3d3919611c3b502169c60
dipinsonwani <dipinsonwani@gmail.com>
2020-12-27 19:31:37 +0530

    Implementation of expand orders

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,13 +1,21 @@
-import 'dart:html';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import '../providers/orders.dart' as ord;
 import 'package:intl/intl.dart';
+import 'dart:math';
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem orders;
 
   OrderItem(this.orders);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -15,15 +23,45 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text('${orders.amount}'),
+            title: Text('₹${widget.orders.amount}'),
             subtitle: Text(
-              DateFormat('dd MM yyyy hh:mm').format(orders.dateTime),
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.orders.dateTime),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
-          )
+          ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15,vertical:4),
+                height: min(widget.orders.products.length * 20.0 + 10, 180),
+                child: ListView(
+                  children: widget.orders.products
+                      .map(
+                        (prod) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                          Text(
+                            prod.title,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          Text(
+                            '${prod.quantity} x ₹${prod.price}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          )
+                        ]),
+                      )
+                      .toList(),
+                ))
         ],
       ),
     );
