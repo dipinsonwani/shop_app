004c79e689e80be7aafff89c4158c9329a392961
dipinsonwani <dipinsonwani@gmail.com>
2020-12-27 15:58:41 +0530

    Add Orders Provider

---
+++ lib/providers/orders.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+import 'package:shop_app/providers/cart.dart';
+
+class OrderItem {
+  final String id;
+  final double amount;
+  final List<CartItem> products;
+  final DateTime dateTime;
+
+  OrderItem({
+    @required this.id,
+    @required this.amount,
+    @required this.dateTime,
+    @required this.products,
+  });
+}
+
+class Orders with ChangeNotifier {
+  List<OrderItem> _orders = [];
+
+  List<OrderItem> get orders {
+    return [..._orders];
+  }
+
+}
