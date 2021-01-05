428818094ee3ecb56d19b2ff79d0048792d3dad4
dipinsonwani <dipinsonwani@gmail.com>
2021-01-05 17:18:37 +0530

    Add EditProductScreen

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,32 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatefulWidget {
+  @override
+  _EditProductScreenState createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Edit product')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                ),
+                textInputAction: TextInputAction.next,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
