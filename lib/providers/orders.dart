fafde5909a1716af6dc7517afb56672ff35fbdf0
dipinsonwani <dipinsonwani@gmail.com>
2021-01-14 14:24:21 +0530

    Add Orders to the server

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:shop_app/providers/cart.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class OrderItem {
   final String id;
@@ -22,15 +24,28 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final timestamp = DateTime.now();
+    const url = 'https://shopapp-12cb0-default-rtdb.firebaseio.com/orders.json';
+    final response = await http.post(url,
+        body: json.encode({
+          'amount': total,
+          'dateTime': timestamp.toIso8601String(),
+          'products': cartProducts.map((cp) => {
+                'id': cp.id,
+                'title': cp.title,
+                'quantity': cp.quantity,
+                'price': cp.price,
+              }).toList()
+        }));
     _orders.insert(
         0,
         OrderItem(
-          id: DateTime.now().toString(),
+          id: json.decode(response.body)['name'],
           amount: total,
-          dateTime: DateTime.now(),
+          dateTime: timestamp,
           products: cartProducts,
         ));
-        notifyListeners();
+    notifyListeners();
   }
 }
