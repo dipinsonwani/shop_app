e9ab6c08bdf0b141ef11cbf0ddb4966b29d0e058
dipinsonwani <dipinsonwani@gmail.com>
2021-01-26 17:53:57 +0530

    Animate order item while expanding orders

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -18,51 +18,55 @@ class _OrderItemState extends State<OrderItem> {
   var _expanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            title: Text('₹${widget.orders.amount}'),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.orders.dateTime),
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: _expanded? min(widget.orders.products.length * 20.0 + 110, 200) : 95,
+          child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text('₹${widget.orders.amount}'),
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.orders.dateTime),
+              ),
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
             ),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 15,vertical:4),
-                height: min(widget.orders.products.length * 20.0 + 10, 180),
-                child: ListView(
-                  children: widget.orders.products
-                      .map(
-                        (prod) => Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                          Text(
-                            prod.title,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18),
-                          ),
-                          Text(
-                            '${prod.quantity} x ₹${prod.price}',
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.grey,
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                height: _expanded? min(widget.orders.products.length * 20.0 + 10, 100) : 0,
+                padding: EdgeInsets.symmetric(horizontal: 15,vertical:4),
+                  child: ListView(
+                    children: widget.orders.products
+                        .map(
+                          (prod) => Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                            Text(
+                              prod.title,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18),
                             ),
-                          )
-                        ]),
-                      )
-                      .toList(),
-                ))
-        ],
+                            Text(
+                              '${prod.quantity} x ₹${prod.price}',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey,
+                              ),
+                            )
+                          ]),
+                        )
+                        .toList(),
+                  ))
+          ],
+        ),
       ),
     );
   }
